3f3c4f309d67a57d6f481ce92b2157707360b6a0
Isa Hu <usunisa.hu@students.codeinstitute.net>
2024-12-30 22:35:23 +0000

    initial commit

---
+++ codestar/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+from blog import views as blog_views
+
+urlpatterns = [
+    path("", blog_views.my_blog, name='blog'),
+    path('admin/', admin.site.urls),
+]
