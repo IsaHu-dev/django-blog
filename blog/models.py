b9d9e3ef3b073711e0f6ccbc754edbc26f9a6a76
Isa Hu <usunisa.hu@students.codeinstitute.net>
2025-01-01 20:05:14 +0000

    add comments to blog

--- blog/models.py
+++ blog/models.py
@@ -26,16 +26,11 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE,
-                             related_name="comments")
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="commenter")
+    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
 
-    class Meta:
-        ordering = ["created_on"]
-
     def __str__(self):
-        return f"Comment {self.body} by {self.author}"
+        return f"Comment by {self.author} on {self.post}"
